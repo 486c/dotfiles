ad0ef30bd5f02d190b5a2225d7d71e3f72ee345a
486c <lopihavefun@gmail.com>
2023-02-05 09:39:15 +0300

    clean up osu-run script

--- local/scripts/osu-run.sh
+++ local/scripts/osu-run.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 #export PATH="$HOME/.wineosu/osuwine/bin:$PATH" #Use custom WINE version to run osu!
-export PATH="/opt/wine-osu/bin:$PATH"
+#export PATH="/opt/wine-osu/bin:$PATH"
+export PATH="$HOME/wine-osu/bin:$PATH"
+#export PATH="$HOME/wine-custom/bin"
 export WINEARCH=win32
 export WINEPREFIX="$HOME/.wineosu"
 #export WINEPREFIX="$HOME/Downloads/opt/wine-osu/"
-#export WINEFSYNC=1
+export WINEFSYNC=1
 
 #VSync. For some reason, some people had been getting input latency issues and for some reason, the fix is to set VSync to off.
 export vblank_mode=0 #For AMD, Intel and others
 export __GL_SYNC_TO_VBLANK=0 #For NVIDIA proprietary and open source >=500
+export WINE_GL_LATENCY_REDUCTION=1
+export WINE_OPENGL_LATENCY_REDUCTION=1
 
-#export STAGING_AUDIO_DURATION=13333 # 0.667ms at 48KHz
+export STAGING_AUDIO_DURATION=13333
+export STAGING_AUDIO_PERIOD=13333
 #export STAGING_AUDIO_PERIOD=13333 # 0.667ms at 48KHz
 #export STAGING_AUDIO_DURATION=26666 # 0.667ms at 48KHz
 #export STAGING_AUDIO_PERIOD=26666 # 0.667ms at 48KHz
 #start osu!
 
 #cd "$HOME/hdd/osu!/"
-
 
 cd "$HOME/hdd/osunew/"
 #cd "$HOME/hdd/osu!/"
