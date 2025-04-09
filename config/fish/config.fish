a4320482e562ea8a43f3d7b2e79acfc42852b875
486c <lopihavefun@gmail.com>
2025-04-09 15:31:09 +0300

    fix(fish): update fish configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.cargo/bin /home/lopij/.local/bin $PATH
 set EDITOR nvim
 
 #export EDITOR=nvim
@@ -11,3 +11,7 @@ alias lg="lazygit"
 alias ntr="cargo-nextest ntr"
 alias ls="eza"
 alias jpaste="wl-paste | jless"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
